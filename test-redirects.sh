7312220b685cf4e6950dfab80965f8836ff64259
Steve Pedersen <speder@pacbell.net>
2014-07-16 08:48:17 -0700

    initial code commit

---
+++ test-redirects.sh
@@ -0,0 +1,90 @@
+#-------------------------------------------------------------------------------
+# Purpose: Bulk test HTTP redirects
+#
+# Dependencies: cURL
+#
+# Example:
+#-------------------------------------------------------------------------------
+
+#   . ~/bin/test-redirects.sh
+#
+#   test_redirect http://example.com/foo/bar http://foo.example.com/bar
+
+#-------------------------------------------------------------------------------
+# Example: multiple, similar URLs
+#-------------------------------------------------------------------------------
+
+#   . ~/bin/test-redirects.sh
+#
+#   base_url=http://example.com
+#
+#   for send_and_expect in \
+#       "foo bar" \
+#       "fizz boom"
+#   do
+#       send=`  echo $send_and_expect | cut -f1 -d" "`
+#       expect=`echo $send_and_expect | cut -f2 -d" "`
+#       test_redirect $base_url/$send $base_url/$expect
+#   done
+#
+#   print_summary
+
+#-------------------------------------------------------------------------------
+# Example: multiple, dissimilar URLs
+#-------------------------------------------------------------------------------
+
+#   . ~/bin/test-redirects.sh
+#
+#   for send_and_expect in \
+#       "http://example.com/foo/bar http://foo.example.com/bar" \
+#       "http://example.com/login https://secure.example.com/sign-in"
+#   do
+#       split_and_test_redirect $send_and_expect
+#   done
+#
+#   print_summary
+
+succeeded=0
+failed=0
+total=0
+
+test_redirect() {
+    send=$1
+    expect=$2
+
+    echo -n $send
+
+    curl_out=`curl -sL -w "%{http_code}\t%{url_effective}" -o /dev/null $send`
+
+    http_code=`echo "$curl_out" | cut -f1`
+    receive=`  echo "$curl_out" | cut -f2`
+
+    total=`expr $total + 1`
+
+    if [ "$http_code" != "200" ]; then
+      echo " ERROR ($http_code) $receive"
+      failed=`expr $failed + 1`
+      return 1
+
+    elif [ -n "$expect" ] && [ "$receive" != "$expect" ]; then
+      echo " ERROR ($http_code) $receive"
+      failed=`expr $failed + 1`
+      return 1
+
+    fi
+
+    echo " --> $receive"
+    succeeded=`expr $succeeded + 1`
+    return 0
+}
+
+split_and_test_redirect() {
+    send=`  echo $1 | cut -f1 -d" "`
+    expect=`echo $1 | cut -f2 -d" "`
+
+    test_redirect $send $expect
+}
+
+print_summary() {
+    echo "Succeeded: $succeeded, Failed: $failed, Total: $total"
+}
